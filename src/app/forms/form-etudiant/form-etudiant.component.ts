9355eee97db6d57fff9bdb33a8d458ba101b7695
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-03-21 14:23:19 +0100

    FIn du TP, ajustement de certain code pour optimisation

--- src/app/forms/form-etudiant/form-etudiant.component.ts
+++ src/app/forms/form-etudiant/form-etudiant.component.ts
@@ -25,18 +25,10 @@ export class FormEtudiantComponent implements OnInit {
   }
   login() {
     if (this.etudiantForm.valid) {
-      const data = {
-        nom: this.etudiantForm.get('nom')?.value,
-        prenom: this.etudiantForm.get('prenom')?.value,
-        rue: this.etudiantForm.get('rue')?.value,
-        cp: this.etudiantForm.get('cp')?.value,
-        ville: this.etudiantForm.get('ville')?.value,
-        courriel: this.etudiantForm.get('courriel')?.value,
-        genre: this.etudiantForm.get('genre')?.value,
-        age: this.etudiantForm.get('age')?.value,
-      };
-
-      localStorage.setItem('NewEtudiantForm', JSON.stringify(data));
+      localStorage.setItem(
+        'NewEtudiantForm',
+        JSON.stringify(this.etudiantForm)
+      );
     }
   }
 }
--- src/app/forms/form-specialite/form-specialite.component.ts
+++ src/app/forms/form-specialite/form-specialite.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-specialite',
   templateUrl: './form-specialite.component.html',
-  styles: ``
+  styles: ``,
 })
-export class FormSpecialiteComponent {
+export class FormSpecialiteComponent implements OnInit {
+  constructor() {}
 
+  SpeForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.SpeForm = new FormGroup({
+      libelle: new FormControl(null, Validators.required),
+      idSpec: new FormControl(null, Validators.required),
+    });
+  }
+
+  login() {
+    if (this.SpeForm.valid) {
+      localStorage.setItem(
+        'form-specialite',
+        JSON.stringify(this.SpeForm.value)
+      );
+    }
+  }
 }
